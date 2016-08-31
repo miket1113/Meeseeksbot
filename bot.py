7882c7f18c428dcc41f843908e056dc911035119
mat8367 <mat8367@g.rit.edu>
2016-08-31 19:23:33 -0400

    Added some stuff

--- bot.py
+++ bot.py
@@ -1,1 +1,14 @@
 #Author Mike Terranova
+
+import time
+import os
+import sys
+import re
+import urllib
+
+#might be using a class? Dunno, maybe just a main function that'll use all our functions
+
+def joinGroupMe():
+    botid = "Mr.Meeseeeks"
+    connectmsg = "Look at me! I'm Mr.Meeseeks!"
+
