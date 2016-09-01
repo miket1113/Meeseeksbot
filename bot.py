2fb466221b69b6c7797f375eec6751cb6249dd07
miket1113 <mat8367@g.rit.edu>
2016-08-31 22:50:30 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,10 +5,28 @@ import os
 import sys
 import re
 import urllib
+import json
+import requests
 
 #might be using a class? Dunno, maybe just a main function that'll use all our functions
 
-def joinGroupMe():
-    botid = "Mr.Meeseeeks"
-    connectmsg = "Look at me! I'm Mr.Meeseeks!"
+class Bot:
+	def _init_():
+		self.groupID = 	
+		self.botID = 	
+def message(request):
+	text = body['text']
+	text = text.lower()
+	if 'meseeks' in text:
+		args = text.split('')
+		groupID = 
+		botID = 
+		msgOut = {'bot_id':botID, 'text' : " Look at me! I'm Mr.Meseeks"}
+		print("sending message")
+		r = requests.post("", data = msgOut)
+
+def start():
+	Bot._init_()
+	while 1:
+		message()
 
